ab1cfaa3ea29026abdcab0ba09037bc09ca86d4a
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-07-23 02:09:52 +0530

    Add files via upload

---
+++ client/pages/media.tsx
@@ -0,0 +1,405 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { LuSmartphone } from "react-icons/lu";
+import { MdMailOutline } from "react-icons/md";
+import img from "../public/ball_outline_24.jpg";
+import img1 from "../public/narrative_active_outline_24.jpg";
+import img2 from "../public/bank_outline_24.jpg";
+import img3 from "../public/employee_outline_24.jpg";
+import img4 from "../public/game_outline_24.jpg";
+import img5 from "../public/education_outline_24.jpg";
+import img6 from "../public/car_outline_24.jpg";
+import img7 from "../public/health_outline_24.jpg";
+import img8 from "../public/bowl_sticks_outline_24.jpg";
+import img9 from "../public/market_outline_24.jpg";
+import img10 from "../public/live_outline_24.jpg";
+import img11 from "../public/video_outline_24.jpg";
+import img12 from "../public/work_outline_24.jpg";
+import img13 from "../public/magic_wand_outline_24.jpg";
+import img14 from "../public/video_circle_outline_24.jpg";
+import img15 from "../public/work_outline_24.jpg";
+import img16 from "../public/place_outline_24.jpg";
+import Image from "next/image";
+
+function Media() {
+  return (
+    <div className="b1">
+      <div className="b2">
+        <span>Developed more than 100 projects in 15 industries</span>
+      </div>
+      <div className="b3">
+        <div className="b3a">
+          <div className="b3a1 a">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="icon">
+                      <Image className="img9" src={img1} alt="img9"></Image>
+                    </div>
+                  </td>
+                  <td>
+                    <span className="b3a1a">Social media</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="b3a1 a">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="icon">
+                      <Image className="img9" src={img} alt="img9"></Image>
+                    </div>
+                  </td>
+                  <td>
+                    <span className="b3a1a">Fitness and sport</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="b3a1 a">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="icon">
+                      <Image className="img9" src={img2} alt="img9"></Image>
+                    </div>
+                  </td>
+                  <td>
+                    <span className="b3a1a">Bank</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="b3a1 a">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="icon">
+                      <Image className="img9" src={img3} alt="img9"></Image>
+                    </div>
+                  </td>
+                  <td>
+                    <span className="b3a1a">Construction</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="b3a1 a">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="icon">
+                      <Image className="img9" src={img4} alt="img9"></Image>
+                    </div>
+                  </td>
+                  <td>
+                    <span className="b3a1a">Game project</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div className="b3b">
+          <div className="b3a">
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img5} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">Education</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img6} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">Auto transport</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img7} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">Medicine,health</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img8} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">Restraunt,food delivery</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img9} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">Marketplaces</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="b3c">
+          <div className="b3a">
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img10} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">AR technology</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img11} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">TV serials</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img12} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">Startups</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img13} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">Religion</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="b3a1 a">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div className="icon">
+                        <Image className="img9" src={img14} alt="img9"></Image>
+                      </div>
+                    </td>
+                    <td>
+                      <span className="b3a1a">Online cources</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="c1">
+        <div className="c2">
+          <div className="c3">
+            <span className="c3a">Projects we are proud of</span>
+            <br></br>
+            <span className="c3b">
+              Our software development company is truly proud of the wonderful
+              clients we have worked with. We enjoy a long-term partnership
+            </span>
+          </div>
+        </div>
+        <div className="d1a">
+          <div className="d1b">
+            <div className="d1c">
+              <div className="d1d">
+                <span className="d1d1">Project 1</span>
+              </div>
+              <div className="d1da">
+                <span className="d1d2">Project 2</span>
+              </div>
+              <div className="d1db">
+                <span className="d1d2">Project 3</span>
+              </div>
+              <div className="d1dc">
+                <span className="d1d2">Project 4</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <a href="/">
+              <img
+                className="project-image"
+                src="https://s3-alpha-sig.figma.com/img/6781/6a28/82ad7f15e26edcf5629068de100f570f?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bHGCqwJbbFDCPN-2tjCdCBCa9Cn1lJxb2eFJi2rcn5GROSZgj7b9EXKpc-flqFCYfnj3kqVfZOqg2PkWcgQPkSXwQgKHHSKV1tBWY5dUEI-CAu7pJOK6bUWMH7rYvt1ei~3w0kGScc8Vj4Jq39-4XgB1yLV8xn0BeKYe-QC42FcMrN8leQcWXTPhakCQf5vJybU5pLb7Qvhtg~6wO~Rp3pXs3UkJHDs4zU~L20liJk5ucX~14jUltk7H9PeshZLXzos3zJ2eo3vhjitm4B6MgWSeW05aJ7P1zmFhR2SkwpxR0dqzSRXqDpYWkADZNasPXW1vEopz0WEmaFadYmqu5A__"
+                alt="bn45"
+              />
+            </a>
+          </div>
+          <div className="e1a">
+            <span className="e1a1">Project 1</span>
+            <br></br>
+            <span className="e2a">
+              Crafted an innovative rental property management app, seamlessly
+              integrating secure login, absence registration, and a tenant
+              notice board. Elevating the resident experience with user-friendly
+              design and efficient communication channels
+            </span>
+            <br></br>
+            <span className="e2b">
+              Business analysis / iOS / Android / QA / UI/UX Design
+            </span>
+            <br></br>
+            <div className="f1">
+              <div className="f1a">
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div className="icon1">
+                          <Image className="img" src={img16} alt="img9"></Image>
+                        </div>
+                      </td>
+                      <td>
+                        <span className="f1a1">India</span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="f2a">
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div className="icon2">
+                          <Image
+                            className="img9"
+                            src={img15}
+                            alt="img9"
+                          ></Image>
+                        </div>
+                      </td>
+                      <td>
+                        <span className="f1a2">Real Estate</span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="g1">
+              <div className="g1a">
+                <span className="g1a1">400%</span>
+                <br></br>
+                <span className="g1a2">User Growth</span>
+              </div>
+              <div className="g2a">
+                <span className="g2a2">+ 200 000</span>
+                <br></br>
+                <span className="g2a3">Active Users</span>
+              </div>
+            </div>
+            <a href="/">
+              <img
+                className="bn46"
+                src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+                alt="bn45"
+              />
+            </a>
+            <a href="/">
+              <img
+                className="bn45"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png"
+                alt="bn45"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Media;
