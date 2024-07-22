531b93090c4779a5cb764ff83e13c7f6d8aa213c
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-07-21 19:52:47 -0700

    Update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -33,7 +33,7 @@ import {
 export default function Home() {
   const [val, setVal] = useState('');
   return (
-    <div className="web1">
+    <>
       <div className="@apply w-[1179px] h-[92px] gap-20 opacity-[0px] bg-white z-[58] fixed ml-20 mt-6 px-6 py-4 rounded-[12px_12px_12px_12px]">
         <img
           className="@apply w-[174px] h-[170px] gap-0 opacity-[0px] absolute -top-10"
@@ -781,7 +781,7 @@ export default function Home() {
 
 
         </div>
-      </div>
     </div>
+    </>
   );
 }
