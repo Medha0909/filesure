702ed9fc18f1f8b16e87da965a6da7859373e338
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2024-07-22 18:03:58 +0530

    Add files via upload

---
+++ client/pages/faq.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { GoPlus } from "react-icons/go";
+import { RxCross2 } from "react-icons/rx";
+
+
+function Faq() {
+  return (
+    <div className='  @apply w-[1440px] h-[972px] gap-10 relative px-[108px] py-20 top-[2470px]'>
+    <div className='  @apply w-[600px] h-[52px] gap-8'>
+      <span className='  @apply w-[600px] h-[52px] gap-0 text-4xl font-bold leading-[51.84px] tracking-[0.32645100355148315px] text-left text-[#101828] absolute top-10'>FAQ</span>
+      </div>  
+      <div className="  @apply w-[1224px] h-[420px] gap-6 absolute top-[150px]">
+      <div className="  @apply w-[600px] h-[232px] gap-4 border-t-[0.5px] border-t-[#D0D5DD] border-solid">
+      <div className="  @apply w-[600px] h-[84px] gap-4 text-xl font-semibold leading-6 tracking-[0.3799999952316284px] text-left text-[#101828] absolute px-0 py-3 border-b-[0.5px] border-b-[#D0D5DD] border-solid top-5">
+      <span className='  @apply w-[524px] h-6 gap-0 text-lg font-bold leading-6 tracking-[0.3799999952316284px] text-left text-[#101828] absolute top-5'>What is the cost of a mobile application?</span>
+      <button className='  @apply w-[60px] h-[60px] gap-2.5 absolute shadow-[0px_2px_4px_0px_#00000014] pt-3 pb-0 px-0 rounded-[50px] left-[525px] top-0 text-myColors'>
+      <GoPlus className="@apply ml-[13px] mb-2.5" size={34}/>
+              </button>
+      </div>
+      <div className="  @apply w-[600px] h-[84px] gap-4 text-xl font-semibold leading-6 tracking-[0.3799999952316284px] text-left text-[#101828] absolute px-0 py-3 border-b-[0.5px] border-b-[#D0D5DD] border-solid top-[120px]">
+      <span className='  @apply w-[524px] h-6 gap-0 text-lg font-bold leading-6 tracking-[0.3799999952316284px] text-left text-[#101828] absolute top-5'>Do you provide a guarantee for the mobile application?</span>
+      <button className='@apply w-[60px] h-[60px] gap-2.5 absolute shadow-[0px_2px_4px_0px_#00000014] pt-3 pb-0 px-0 rounded-[50px] left-[525px] top-0 text-myColors'>
+      <GoPlus className="  @apply ml-[13px] mb-2.5" size={34}/>
+              </button>
+      </div>
+</div>
+      
+<div className="  @apply w-[600px] h-[420px] gap-4 absolute border-t-[0.5px] border-t-[#D0D5DD] border-solid left-[630px] top-0">
+      <div className="  @apply w-[600px] h-[272px] gap-4 text-xl font-semibold leading-6 tracking-[0.3799999952316284px] text-left text-[#101828] absolute px-0 py-3 border-b-[0.5px] border-b-[#D0D5DD] border-solid top-5">
+      <span className='  @apply w-[524px] h-6 gap-0 text-lg font-bold leading-6 tracking-[0.3799999952316284px] text-left text-[#101828] absolute top-5'>How long will development take?</span>
+      <div className="  @apply absolute top-[60px]">
+      <div className='  @apply w-[524px] h-20 gap-0 text-base font-semibold leading-5 tracking-[-0.3199999928474426px] text-left'>Development terms directly depend on the requirements for the mobile application, the characteristics of the company, as well as the wishes of the customer.</div>
+      <div className='  @apply w-[524px] h-20 gap-0 text-base font-semibold leading-5 tracking-[-0.3199999928474426px] text-left'>
+Average development time from start to finished application:
+Medium projects up to 3 months.
+Large projects about 4-6 months.
+To get a more accurate estimate of the project completion time, it is necessary to determine the main task of the application, think over its logic and functionality.</div>
+</div>
+<button className='@apply w-[60px] h-[60px] gap-2.5 absolute shadow-[0px_2px_4px_0px_#00000014] pt-3 pb-0 px-0 rounded-[50px] left-[525px] top-0 text-myColorssss'>
+      <RxCross2 className="  @apply ml-[13px] mb-2.5" size={30}/>
+              </button>
+      </div>
+      <div className="  @apply w-[600px] h-[84px] gap-4 text-xl font-semibold leading-6 tracking-[0.3799999952316284px] text-left text-[#101828] absolute px-0 py-3 border-b-[0.5px] border-b-[#D0D5DD] border-solid top-[310px]">
+      <span className='  @apply w-[524px] h-6 gap-0 text-lg font-bold leading-6 tracking-[0.3799999952316284px] text-left text-[#101828] absolute top-5'>I will not tell my idea, do you guarantee confidentiality?</span>
+      <button className='@apply w-[60px] h-[60px] gap-2.5 absolute shadow-[0px_2px_4px_0px_#00000014] pt-3 pb-0 px-0 rounded-[50px] left-[525px] top-0 text-myColors'>
+      <GoPlus className="  @apply ml-[13px] mb-2.5" size={34}/>
+              </button>
+      </div>
+
+
+      
+      </div>
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Faq;
---
+++ client/pages/media.tsx
@@ -0,0 +1,145 @@
+import React from "react";
+
+function Media() {
+  return (
+    <div className="media bg-myColor">
+      <h1>
+        Developed more than{" "}
+        <span className="highlight">
+          100
+          <br />
+        </span>{" "}
+        projects in <span className="highlight">15</span> industries
+      </h1>
+      <ul>
+        <li>
+          <span className="icon" /> Social media &nbsp; &nbsp;{" "}
+          <span className="icon" /> Fitness and spor <span className="icon" />{" "}
+          Bank
+        </li>
+        <li>
+          <span className="icon">
+            <i className="fas fa-tools" />
+          </span>{" "}
+          Construction &nbsp; &nbsp;{" "}
+          <span className="icon">
+            <i className="fas fa-gamepad" />
+          </span>{" "}
+          Game projects &nbsp; &nbsp;{" "}
+          <span className="icon">
+            <i className="fas fa-graduation-cap" />
+          </span>{" "}
+          Education
+        </li>
+        <li>
+          <span className="icon">
+            <i className="fas fa-car" />
+          </span>{" "}
+          Auto, transport{" "}
+          <span className="icon">
+            <i className="fas fa-heart" />
+          </span>{" "}
+          Medicine, health &nbsp;{" "}
+          <span className="icon">
+            <i className="fas fa-utensils" />
+          </span>{" "}
+          Restaurants, food delivery
+        </li>
+        <li>
+          <span className="icon">
+            <i className="fas fa-home" />
+          </span>{" "}
+          Marketplaces &nbsp;{" "}
+          <span className="icon">
+            <i className="fas fa-satellite-dish" />
+          </span>{" "}
+          AR technology &nbsp; &nbsp; &nbsp;{" "}
+          <span className="icon">
+            <i className="fas fa-tv" />
+          </span>{" "}
+          TV series
+        </li>
+        <li>
+          <span className="icon">
+            <i className="fas fa-rocket" />
+          </span>{" "}
+          Startups &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;{" "}
+          <span className="icon">
+            <i className="fas fa-praying-hands" />
+          </span>{" "}
+          Religion &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          &nbsp;{" "}
+          <span className="icon">
+            <i className="fas fa-book-open" />
+          </span>{" "}
+          Online courses
+        </li>
+      </ul>
+      <h1>
+        Projects we are proud of{" "}
+        <img
+          src="https://s3-alpha-sig.figma.com/img/6781/6a28/82ad7f15e26edcf5629068de100f570f?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PEH1QzvhuS~mfH4fJI9HMQSfXLXdjXyZxCaholt4jIqdWwyuLn6NXhM8It37opwulxFcLqxrynThlwAR4U0H1t3wqCBJmSYut3QwlvBePYge3Oc-PodE2T6zI7Iyu7fv-G2Z2erlDHs~4EyAihP8i85wf7qXPThrlPBdW-RYtk2Pz81kQs82YSXfq4LxhDEq4UjhA9zDmWL5G5jhy3dTzcmMDsIqoZu9jGTNr4swBl7gPFOq7w4K4urOOtIT-OL4VZ-QmLtHP0C3Mm653JSIr-aSnBu~QGagDQTNRvDYrcg3JQvqrLjUMe4nZYW~H~WibPrAmNBSFUiDKjQOopNtoQ__"
+          alt="Project image"
+          className="project-image"
+        />
+      </h1>
+      <p>
+        Our software development company is truly proud of the wonderful clients
+        we have
+        <br /> worked with. We enjoy a long-term partnership
+      </p>
+      <table>
+        <tbody>
+          <tr>
+            <td>Project 1</td> <td>Project 2</td> <td>Project 3</td>{" "}
+            <td>Project 4</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2>Project 1</h2>
+      <span className="project">
+        <p>
+          Crafted an innovative rental property management app, seamlessly
+          integrating secure
+          <br /> login, absence registration, and a tenant notice board.
+          Elevating the resident
+          <br /> experience with user-friendly design and efficient
+          communication channels
+        </p>
+      </span>
+      <span className="business">
+        <p>Business analysis / iOS / Android / QA / UI/UX Design</p>
+      </span>
+      <span className="india">India</span>
+      <span className="Real">Real Estate</span>
+      <div className="stats">
+        <div className="stat">
+          <span className="stat-value">400%</span>
+          <span className="stat-value">+ 200 000</span>
+        </div>
+      </div>
+      <div className="stats">
+        <div className="stat">
+          <span className="stat-label">User Growth</span>
+          <span className="stat-label">Active Users</span>
+        </div>
+      </div>
+      <a href="/" tabIndex={0}>
+        <img
+          className="bn46"
+          src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+          alt="bn45"
+        />
+      </a>
+      <a href="/" tabIndex={0}>
+        <img
+          className="bn45"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png"
+          alt="bn45"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default Media;
